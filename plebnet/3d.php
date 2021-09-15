85325747609eee0e89ba0e3ea79eea4030577232
Richard Safier <richardj@safier.com>
2021-09-14 21:55:47 -0400

    tweaks

--- plebnet/3d.php
+++ plebnet/3d.php
@@ -38,22 +38,7 @@ include 'includes/favicon.html';
 <script src="https://unpkg.com/3d-force-graph"></script>
 </head>
 <body>
-	<script src="/assets/js/donate.js?v=1.2.2"></script>
-	<script src ="https://btcpay.satoshis.tech/modal/btcpay.js"></script>
-	<form method="POST"  onsubmit="onBTCPayFormSubmit(event);return false"  action="https://btcpay.satoshis.tech/api/v1/invoices">
-		<input type="hidden" name="checkoutDesc" value="Donation from Anonymous" />
-		<input type="hidden" name="price" value="0.00001" />
-		<input type="hidden" name="storeId" value="BhtTLhbEhSZByquLhPqbbG45SejRXHkimRtBUbtn7m7y" />
-		<input type="hidden" name="jsonResponse" value="true" />
-		<input type="hidden" name="notifyEmail" value="donation@satoshis.tech" />
-		<input type="hidden" name="currency" value="BTC" />
-		<button type="submit" name="submit" id="donate">
-			<img src="assets/img/icons/donate_24.png">Buy me a coffee
-		</button>
-	</form>
-
 	<div id="3d-graph"></div>
-
 <script>
 <?php
 include 'includes/graph3d.js';
